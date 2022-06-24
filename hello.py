09118ccb2b4749a2e8ebe57d04eba545b885a234
Jorge Borge <jorgealbertoborge@gmail.com>
2022-06-24 13:11:08 -0400

    Add Search and WTF forms

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key that now know one is supposed to know"
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # Create a route decorator 
 @app.route('/')
@@ -28,12 +37,25 @@ def user(name):
 # Create Custom Error Pages
 
 # Invalid URL 
-
-# Internal Server Error 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
 
+# Internal Server Error 
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form 
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template("name.html", 
+        name=name,
+        form=form)
